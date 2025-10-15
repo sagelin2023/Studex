54e936a47737dc30194c46fb0db7124a81cc7d52
sagelin27 <sagelin2023@gmail.com>
2025-10-15 17:45:37 -0400

    set up app and installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
